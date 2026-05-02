f20b49b7289ab877b233113d115b8ed99e68d642
medhamishra24 <mishramedha976@gmail.com>
2026-05-03 01:46:27 +0530

    fix backend response

--- backend/server.js
+++ backend/server.js
@@ -73,6 +73,10 @@ app.delete("/delete/:id", async (req, res) => {
 // ===== START SERVER =====
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+    res.send("Backend is running 🚀");
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
