3a9301448a0656af61c3c824be034e1434aed10f
medhamishra24 <mishramedha976@gmail.com>
2026-05-03 14:20:06 +0530

    fixed mongodbatlas

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/taskmanager")
+mongoose.connect("mongodb+srv://mishramedha976:medha123@cluster0.tgx3ieh.mongodb.net/taskmanager")
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
